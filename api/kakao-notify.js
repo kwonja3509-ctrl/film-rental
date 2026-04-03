d4d2dbee40566d5cfbf6a3a5ef0a6c314e62cc15
kwonja3509-ctrl <kwonja3509@gmail.com>
2026-04-04 02:40:49 +0900

    카카오톡

--- api/kakao-notify.js
+++ api/kakao-notify.js
@@ -1,3 +1,4 @@
+// 카카오톡 나에게 보내기 알림
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
